40dadb85f1eb88d703dabb7711eaf4702c699dc5
tsullivan <thomas.sullivan@lookout.com>
2014-11-25 01:30:45 -0800

    adding summarizer for simplecov

---
+++ simplecov_summarizer.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+var simplecovResultFilePath = process.argv[2];
+if(simplecovResultFilePath==null) throw new Error("usage: node simplecov_summarizer path/to/simplecov/output.json");
+
+var simpleCovResultString = fs.readFileSync(simplecovResultFilePath).toString();
+var simpleCovJSON = JSON.parse(simpleCovResultString);
+
+var linesCovered = 0;
+var eligibleLines = 0;
+var coverageHash = simpleCovJSON.details.RSpec.coverage;
+for(var key in coverageHash) {
+    var lineCoverages = coverageHash[key]
+    lineCoverages.forEach(function(lineCoverage) {
+       if(lineCoverage==null) return;
+       if(lineCoverage>0) linesCovered++;
+        eligibleLines++;
+    });
+}
+
+simpleCovJSON.lines = {
+    percent: linesCovered / eligibleLines,
+    covered: linesCovered,
+    total: eligibleLines
+}
+
+console.log(JSON.stringify(simpleCovJSON));
